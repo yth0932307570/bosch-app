2c977d6e1970a4dc1dab3d3728f957202b9c69ff
yth0932307570 <yth0932307570@gmail.com>
2018-01-12 10:28:38 +0800

    no message

---
+++ Assets/Main.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void Onclick(){
+	//查　　開啟網頁的程式與法
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
